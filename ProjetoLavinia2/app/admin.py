2626426b644e143274431aad3303eb36c65d90f7
Lavissantos <161363455+Lavissantos@users.noreply.github.com>
2024-10-21 16:00:11 -0300

    Delete ProjetoLavinia2 directory

--- ProjetoLavinia2/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Cidade)
-admin.site.register(Preferencias)
-admin.site.register(Avaliacao)
-admin.site.register(Time)
-admin.site.register(Estadio)
