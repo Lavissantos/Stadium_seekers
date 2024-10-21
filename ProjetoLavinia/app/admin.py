01dfa44183d4e9c0b5eef0fb8c225c9c6108f9a2
Lavissantos <161363455+Lavissantos@users.noreply.github.com>
2024-10-21 16:00:02 -0300

    Delete ProjetoLavinia directory

--- ProjetoLavinia/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Time, Estadio, Usuario, Avaliacao, Ingresso, Quiz  # Importar todos os modelos que você deseja registrar
-
-admin.site.register(Time)
-admin.site.register(Estadio)
-admin.site.register(Usuario)
-admin.site.register(Avaliacao)
-admin.site.register(Ingresso)
-admin.site.register(Quiz)
